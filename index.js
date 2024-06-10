33f73f4b385fa7f589317ed9bf5c82c9f4ca239a
KietNLS <kiet@nlstech.net>
2024-06-10 23:32:59 +0700

    feat: Add route for validating user token

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const login = require("./routes/login");
 const accounts = require("./routes/accounts");
 const history = require("./routes/history");
 const licenseKey = require("./routes/license-key");
+const validate = require("./routes/validate");
 
 // Middlewares
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 // Routes
 app.use("/", home);
 app.use("/api/login", login);
+app.use("/api/validate", validate);
 // app.use("/api/accounts", accounts);
 app.use("/api/history", history);
 // app.use("/api/license-key", licenseKey);
