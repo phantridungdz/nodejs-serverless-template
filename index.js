d26f1f31752e8fa4eaf637b3ec1552b8d5d1a28c
Phant Chase <phantridungdz@gmail.com>
2024-06-10 22:32:11 +0700

    config for Kiet

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // Import packages
+require("dotenv").config();
 const express = require("express");
-const home = require("./routes/home");
+const home = require("./routes/index");
+const login = require("./routes/login");
+const accounts = require("./routes/accounts");
+const history = require("./routes/history");
+const licenseKey = require("./routes/license-key");
 
 // Middlewares
 const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/home", home);
+app.use("/", home);
+// app.use("/api/login", login);
+// app.use("/api/accounts", accounts);
+// app.use("/api/history", history);
+// app.use("/api/license-key", licenseKey);
 
 // connection
 const port = process.env.PORT || 9001;
