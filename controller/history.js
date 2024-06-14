d4a4d638de3c8d17343056785190801cab2000bc
KietNLS <kiet@nlstech.net>
2024-06-14 19:18:13 +0700

    chore: Refactor history controller and routes

--- controller/history.js
+++ controller/history.js
@@ -59,6 +59,9 @@ const getHistoryData = async (req, res) => {
   });
 };
 
+
+//analytics
+//not by key
 const getThisWeekData = async () => {
   const startDateThisWeek = moment().startOf("week").toDate();
   const endDateThisWeek = moment().endOf("week").toDate();
@@ -108,11 +111,17 @@ const getThisMonthData = async () => {
   console.log("data", data);
   console.log("error", error);
   return data;
-}
+};
 
 const getLastMonthData = async () => {
-  const startDateLastMonth = moment().subtract(1, "month").startOf("month").toDate();
-  const endDateLastMonth = moment().subtract(1, "month").endOf("month").toDate();
+  const startDateLastMonth = moment()
+    .subtract(1, "month")
+    .startOf("month")
+    .toDate();
+  const endDateLastMonth = moment()
+    .subtract(1, "month")
+    .endOf("month")
+    .toDate();
   //convert moment to Date
   const db = supabase();
   let dataPromise = db.rpc("get_range_history_money", {
@@ -124,7 +133,7 @@ const getLastMonthData = async () => {
   console.log("data", data);
   console.log("error", error);
   return data;
-}
+};
 
 const getWeekAnalytics = async (req, res) => {
   const thisWeekData = await getThisWeekData();
@@ -152,6 +161,123 @@ const getMonthAnalytics = async (req, res) => {
   });
 };
 
+
+//analytics
+//by key
+const getThisWeekDataByKey = async (key) => {
+  const startDateThisWeek = moment().startOf("week").toDate();
+  const endDateThisWeek = moment().endOf("week").toDate();
+  //convert moment to Date
+  const db = supabase();
+  let dataPromise = db.rpc("get_range_history_money_by_key", {
+    key: key,
+    start_date: startDateThisWeek,
+    end_date: endDateThisWeek,
+  });
+
+  const { data, error } = await dataPromise;
+  console.log("data", data);
+  console.log("error", error);
+  return data;
+};
+
+const getLastWeekDataByKey = async (key) => {
+  const startDateLastWeek = moment()
+    .subtract(1, "week")
+    .startOf("week")
+    .toDate();
+  const endDateLastWeek = moment().subtract(1, "week").endOf("week").toDate();
+  //convert moment to Date
+  const db = supabase();
+  let dataPromise = db.rpc("get_range_history_money_by_key", {
+    key: key,
+    start_date: startDateLastWeek,
+    end_date: endDateLastWeek,
+  });
+
+  const { data, error } = await dataPromise;
+  console.log("data", data);
+  console.log("error", error);
+  return data;
+};
+
+const getThisMonthDataByKey = async (key) => {
+  const startDateThisMonth = moment().startOf("month").toDate();
+  const endDateThisMonth = moment().endOf("month").toDate();
+  //convert moment to Date
+  const db = supabase();
+  let dataPromise = db.rpc("get_range_history_money_by_key", {
+    key: key,
+    start_date: startDateThisMonth,
+    end_date: endDateThisMonth,
+  });
+
+  const { data, error } = await dataPromise;
+  console.log("data", data);
+  console.log("error", error);
+  return data;
+};
+
+const getLastMonthDataByKey = async (key) => {
+  const startDateLastMonth = moment()
+    .subtract(1, "month")
+    .startOf("month")
+    .toDate();
+  const endDateLastMonth = moment()
+    .subtract(1, "month")
+    .endOf("month")
+    .toDate();
+
+  console.log("startDateLastMonth", startDateLastMonth);
+  console.log("endDateLastMonth", endDateLastMonth);
+  //convert moment to Date
+  const db = supabase();
+  let dataPromise = db.rpc("get_range_history_money_by_key", {
+    key: key,
+    start_date: startDateLastMonth,
+    end_date: endDateLastMonth,
+  });
+
+  const { data, error } = await dataPromise;
+  console.log("data", data);
+  console.log("error", error);
+  return data;
+};
+
+const getWeekAnalyticsByKey = async (req, res) => {
+  if (!req.params.key) {
+    return res.status(400).send("Key is required");
+  }
+  const thisWeekData = await getThisWeekDataByKey(req.params.key);
+  const lastWeekData = await getLastWeekDataByKey(req.params.key);
+
+  const percentage = ((thisWeekData - lastWeekData) / lastWeekData) * 100;
+  console.log("percentage", percentage);
+  return res.status(200).json({
+    thisWeekData,
+    lastWeekData,
+    percentage,
+  });
+}
+
+const getMonthAnalyticsByKey = async (req, res) => {
+  if (!req.params.key) {
+    return res.status(400).send("Key is required");
+  }
+  const thisMonthData = await getThisMonthDataByKey(req.params.key);
+  const lastMonthData = await getLastMonthDataByKey(req.params.key);
+
+  const percentage = ((thisMonthData - lastMonthData) / lastMonthData) * 100;
+  console.log("percentage", percentage);
+  return res.status(200).json({
+    thisMonthData,
+    lastMonthData,
+    percentage,
+  });
+}
+
+
+//chart and total money by key
 const getTotalMoneyByKey = async (key) => {
   const db = supabase();
   let dataPromise = db.rpc("get_total_money_by_key", {
@@ -159,7 +285,7 @@ const getTotalMoneyByKey = async (key) => {
   });
   const { data, error } = await dataPromise;
   return { data, error };
-}
+};
 
 const getChartMoneyByKeys = async (key) => {
   const db = supabase();
@@ -168,9 +294,9 @@ const getChartMoneyByKeys = async (key) => {
   });
   const { data, error } = await dataPromise;
   return { data, error };
-}
+};
 
-const getChartAndTotalMoneyByKeys = async (req,res) => {
+const getChartAndTotalMoneyByKeys = async (req, res) => {
   if (!req.params.key) {
     return res.status(400).send("Key is required");
   }
@@ -185,11 +311,15 @@ const getChartAndTotalMoneyByKeys = async (req,res) => {
     chartMoney: chartData.data,
     totalMoney: totalData.data,
   });
-}
+};
+
+
 
 module.exports = {
   getHistoryData,
   getWeekAnalytics,
   getMonthAnalytics,
-  getChartAndTotalMoneyByKeys
+  getChartAndTotalMoneyByKeys,
+  getWeekAnalyticsByKey,
+  getMonthAnalyticsByKey,
 };
--- routes/history.js
+++ routes/history.js
@@ -4,13 +4,17 @@ const {
   getHistoryData,
   getWeekAnalytics,
   getMonthAnalytics,
-  getChartAndTotalMoneyByKeys
+  getChartAndTotalMoneyByKeys,
+  getMonthAnalyticsByKey,
+  getWeekAnalyticsByKey
 } = require("../controller/history");
 const router = express.Router();
 
 router.post("/", checkAuth, getHistoryData);
 router.get("/weekly-analysis", checkAuth, getWeekAnalytics);
 router.get("/monthly-analysis", checkAuth, getMonthAnalytics);
+router.get("/weekly-analysis-key/:key", checkAuth, getWeekAnalyticsByKey);
+router.get("/monthly-analysis-key/:key", checkAuth, getMonthAnalyticsByKey);
 router.get("/money/:key", checkAuth, getChartAndTotalMoneyByKeys);
 
 module.exports = router;
