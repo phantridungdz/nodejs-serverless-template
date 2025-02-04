859c9c946c673d10907795977f47b9054ab4519f
dotuankiet2604 <dotuankiet2604@gmail.com>
2025-02-04 22:48:25 +0700

    fix: import

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { checkAuth } = require("../utils/middleware");
 
 const router = express.Router();
 
